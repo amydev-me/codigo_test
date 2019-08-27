7c1b10dcd10e842fa73c1a260c8d5e8b5083cf00
Amy <ampyaephyonaing@gmail.com>
2019-08-28 01:21:46 +0630

    Modify ImageCard

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,34 +1,27 @@
 import React from 'react';
 
-class ImageCard extends React.Component{
-
-    constructor(props){
-        super(props);
-
-        this.state = { spans: 0 };
-    }
-    render(){            
-        const { space_name, space_description, space_thumb_image, type_name, base_price } = this.props.image;
-        return (
-                <div className="col-12 col-sm-6 col-md-4 col-xl-2 mb-2">                    
-                    <div className="list_item-image" style={{height: "300px"}}>
-                        <img src={ space_thumb_image }
-                            alt={ space_description }  />
+const ImageCard = (props) =>{
+    const { space_name, space_description, space_thumb_image, type_name, base_price } = props.image;
+    return (
+            <div className="col-12 col-sm-6 col-md-4 col-xl-2 mb-2">                    
+                <div className="list_item-image" style={{height: "300px"}}>
+                    <img src={ space_thumb_image }
+                        alt={ space_description }  />
+                    </div>
+                    <div className="list_item">
+                        <div className="list_item-title">
+                            <span>{space_name} </span>
                         </div>
-                        <div className="list_item">
-                            <div className="list_item-title">
-                                <span>{space_name} </span>
-                            </div>
-                            <div className="list_item-txt">
-                                <span>{type_name}</span>
-                            </div>
-                            <div className="list_item-price">
-                                <span style={{fontWeight: "bold"}}>${base_price}</span> / <span>day</span>
+                        <div className="list_item-txt">
+                            <span>{type_name}</span>
                         </div>
+                        <div className="list_item-price">
+                            <span style={{fontWeight: "bold"}}>${base_price}</span> / <span>day</span>
                     </div>
                 </div>
-        );
-    } 
+            </div>
+    );
+    
 }
 
 export default ImageCard;
